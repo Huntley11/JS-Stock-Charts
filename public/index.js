8062568c6aaddcc7a3c9c0ea689b9a6acaa84f78
Huntley <huntleycastner@gmail.com>
2023-02-21 21:29:23 -0800

    added comments

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//Telling the ChartJS the color for each stock to be used
 function getColor(stock){
     if(stock === "GME"){
         return 'rgba(61, 161, 61, 0.7)'
@@ -17,32 +18,38 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+    //awaiting to receive data for stocks
     const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
 
     const result = await response.json()
-
+    //our data
     const { GME, MSFT, DIS, BNTX } = result;
 
     const stocks = [GME, MSFT, DIS, BNTX];
-
+    //changes the dates from descending order to ascending order
     stocks.forEach( stock => stock.values.reverse())
 
     // Time Chart
+    //creating our chart
     new Chart(timeChartCanvas.getContext('2d'), {
+        //changed chart from bar to line
         type: 'line',
         data: {
+            //using map we are getting the values of .datetime
             labels: stocks[0].values.map(value => value.datetime),
             datasets: stocks.map(stock => ({
+                //creating the label using the stocks symbol
                 label: stock.meta.symbol,
                 backgroundColor: getColor(stock.meta.symbol),
                 borderColor: getColor(stock.meta.symbol),
+                //mapping over the values array and taking the highest cost
                 data: stock.values.map(value => parseFloat(value.high))
             }))
         }
     });
 
     // High Chart
+    //creating our bar chart
     new Chart(highestPriceChartCanvas.getContext('2d'), {
         type: 'bar',
         data: {
@@ -55,6 +62,7 @@ async function main() {
                 borderColor: stocks.map(stock => (
                     getColor(stock.meta.symbol)
                 )),
+                //getting our highest values for the chart
                 data: stocks.map(stock => (
                     findHighest(stock.values)
                 ))
@@ -63,6 +71,7 @@ async function main() {
     });
 
     // Average Chart
+    //creating our pie chart
     new Chart(averagePriceChartCanvas.getContext('2d'), {
         type: 'pie',
         data: {
@@ -75,6 +84,7 @@ async function main() {
                 borderColor: stocks.map(stock => (
                     getColor(stock.meta.symbol)
                 )),
+                //getting our Average values for the chart
                 data: stocks.map(stock => (
                     calculateAverage(stock.values)
                 ))
@@ -82,7 +92,7 @@ async function main() {
         }
     });
 }
-
+//function for getting the highest values by comparing our values to see which is the highest 
 function findHighest(values) {
     let highest = 0;
     values.forEach(value => {
@@ -92,7 +102,7 @@ function findHighest(values) {
     })
     return highest
 }
-
+//function to get average by using our total and dividing by the number of values or length
 function calculateAverage(values) {
     let total = 0;
     values.forEach(value => {
